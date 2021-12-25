a00121b8b2ed079d37b5846ad96f8dbbf5ecff39
BassmatiOnMusic <bassmatiOnMusic@freenet.de>
2021-12-25 15:57:57 +0100

    some edits

---
+++ start-dev.bat
@@ -0,0 +1,3 @@
+start /min deno-file-server
+d:\progs\VS2022\Common7\IDE\devenv.com "D:\github\web-technologies\"
+start "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" "http://localhost:4507/svg/index.htm"
