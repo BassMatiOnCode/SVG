0ae4b376e87daea17e6c49d2bbabe1cb2029e4a4
BassmatiOnMusic <bassmatiOnMusic@freenet.de>
2022-02-13 09:57:17 +0100

    Restructured

--- start-dev.bat
+++ start-dev.bat
@@ -1,3 +1,4 @@
+d:\progs\VS2022\Common7\IDE\devenv.com "D:\github\bassmati-on-code\web-technologies\"
+cd docs
 start /min deno-file-server
-d:\progs\VS2022\Common7\IDE\devenv.com "D:\github\web-technologies\"
-start "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" "http://localhost:4507/svg/index.htm"
+start "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" "http://localhost:4507/index.htm"
